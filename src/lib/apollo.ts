135905a0dddcec4c337e953e4b4de8649c8c696c
eliel20matos <eliellimamatos@gmail.com>
2022-06-22 22:35:05 -0400

    feat: adding id from graph cms

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: '',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4q9pqny3t5u01z443z5fjw1/master',
     cache: new InMemoryCache()
 })
