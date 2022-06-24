bd6617997f91a8f266fce2a8e22bacf610dd197a
eliel20matos <eliellimamatos@gmail.com>
2022-06-23 21:08:18 -0400

    feat: adicionando react router dom e provendo rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { gql, useQuery } from "@apollo/client"
+import { ApolloProvider, gql, useQuery } from "@apollo/client"
 import { useEffect } from "react"
+import { BrowserRouter } from "react-router-dom"
 import Header from "./components/Header"
 import Lesson from "./components/Lesson"
 import Sidebar from "./components/Sidebar"
 import Video from "./components/Video"
+import { client } from "./lib/apollo"
 import Evento from "./pages/Evento"
+import Router from "./Router"
 
 function App() {
   return (
-    <div>
-      <Evento/>
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
